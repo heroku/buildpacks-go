9a365734b65e2e7e481600b8f0e736f43c404070
Josh W Lewis <josh.lewis@salesforce.com>
2022-08-01 14:07:08 -0500

    Don't use stack id for dist caching

--- src/layers/dist.rs
+++ src/layers/dist.rs
@@ -1,7 +1,6 @@
 use crate::{tgz, GoBuildpack, GoBuildpackError};
 use heroku_go_buildpack::inv::Artifact;
 use libcnb::build::BuildContext;
-use libcnb::data::buildpack::StackId;
 use libcnb::data::layer_content_metadata::LayerTypes;
 use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
 use libcnb::layer_env::{LayerEnv, ModificationBehavior, Scope};
@@ -20,7 +19,6 @@ pub(crate) struct DistLayer {
 pub(crate) struct DistLayerMetadata {
     layer_version: String,
     go_version: String,
-    stack_id: StackId,
 }
 
 #[derive(Error, Debug)]
@@ -45,7 +43,7 @@ impl Layer for DistLayer {
 
     fn create(
         &self,
-        context: &BuildContext<Self::Buildpack>,
+        _ctx: &BuildContext<Self::Buildpack>,
         layer_path: &Path,
     ) -> Result<LayerResult<Self::Metadata>, GoBuildpackError> {
         log_info(format!("Installing Go {}", self.artifact.semantic_version));
@@ -58,7 +56,7 @@ impl Layer for DistLayer {
         )
         .map_err(DistLayerError::Tgz)?;
 
-        LayerResultBuilder::new(DistLayerMetadata::current(self, context))
+        LayerResultBuilder::new(DistLayerMetadata::current(self))
             .env(
                 LayerEnv::new()
                     .chainable_insert(
@@ -79,10 +77,10 @@ impl Layer for DistLayer {
 
     fn existing_layer_strategy(
         &self,
-        context: &BuildContext<Self::Buildpack>,
+        _ctx: &BuildContext<Self::Buildpack>,
         layer_data: &LayerData<Self::Metadata>,
     ) -> Result<ExistingLayerStrategy, <Self::Buildpack as Buildpack>::Error> {
-        if layer_data.content_metadata.metadata == DistLayerMetadata::current(self, context) {
+        if layer_data.content_metadata.metadata == DistLayerMetadata::current(self) {
             log_info(format!("Reusing Go {}", self.artifact.semantic_version));
             Ok(ExistingLayerStrategy::Keep)
         } else {
@@ -92,10 +90,9 @@ impl Layer for DistLayer {
 }
 
 impl DistLayerMetadata {
-    fn current(layer: &DistLayer, context: &BuildContext<GoBuildpack>) -> Self {
+    fn current(layer: &DistLayer) -> Self {
         DistLayerMetadata {
             go_version: layer.artifact.go_version.clone(),
-            stack_id: context.stack_id.clone(),
             layer_version: String::from(LAYER_VERSION),
         }
     }
