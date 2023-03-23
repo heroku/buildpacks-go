6211445682216a50e6bb20d4245d628f4e7dbbb4
Josh W Lewis <josh.lewis@salesforce.com>
2023-03-23 14:34:34 -0500

    Add inventory automation

---
+++ src/bin/diff_inventory.rs
@@ -0,0 +1,45 @@
+#![warn(clippy::pedantic)]
+
+use heroku_go_buildpack::inv::{list_github_go_versions, Artifact, Inventory};
+use std::collections::HashSet;
+
+/// Prints a human-readable software inventory difference. Useful
+/// for generating commit messages and changelogs for automated inventory
+/// updates.
+fn main() {
+    let inventory_path = std::env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("$ diff_inventory path/to/inventory.toml");
+        std::process::exit(1);
+    });
+
+    let upstream_versions: HashSet<String> = list_github_go_versions()
+        .unwrap_or_else(|e| {
+            eprintln!("Failed to fetch upstream go versions on GitHub: {e}");
+            std::process::exit(1)
+        })
+        .into_iter()
+        .filter_map(|v| Artifact::build(v).ok())
+        .map(|a| a.go_version)
+        .collect();
+
+    let local_versions: HashSet<String> = Inventory::read(&inventory_path)
+        .unwrap_or_else(|e| {
+            eprintln!("Error reading inventory at '{inventory_path}': {e}");
+            std::process::exit(1);
+        })
+        .artifacts
+        .iter()
+        .map(|r| r.go_version.to_string())
+        .collect();
+
+    let mut new_versions: Vec<String> = upstream_versions
+        .difference(&local_versions)
+        .map(String::to_string)
+        .collect();
+
+    new_versions.sort();
+
+    if !new_versions.is_empty() {
+        println!("Added {}.", new_versions.join(", "));
+    }
+}
