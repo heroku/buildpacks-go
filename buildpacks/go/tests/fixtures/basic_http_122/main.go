29d2c6f85ec8f7025fb481d9ed82cb40a33d8d0e
Josh W Lewis <josh.lewis@salesforce.com>
2024-02-08 12:28:16 -0600

    Add tests for go1.22 line

---
+++ buildpacks/go/tests/fixtures/basic_http_122/main.go
@@ -0,0 +1,21 @@
+// +build heroku
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"net/http"
+)
+
+func root(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "basic_http_122")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" { port = "8080" }
+
+	http.HandleFunc("/", root)
+	http.ListenAndServe(":" + port, nil)
+}
