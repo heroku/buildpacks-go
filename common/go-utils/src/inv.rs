82eb869295b5b6cf44ac7b7c98506a371964b0c1
Rune Soerensen <rsoerensen@salesforce.com>
2024-02-26 23:47:05 -0500

    Add comment on parsing error handling

--- common/go-utils/src/inv.rs
+++ common/go-utils/src/inv.rs
@@ -109,6 +109,7 @@ pub fn list_upstream_artifacts() -> Result<Vec<Artifact>, String> {
         .filter_map(|t| {
             t.get_go_release_file().map(|gofile| Artifact {
                 go_version: t.version.clone(),
+                // Handle semver parsing errors elsewhere
                 semantic_version: Version::parse_go(&t.version)
                     .expect("Go version couldn't be semver parsed"),
                 architecture: gofile.get_target_arch(),
