0dee3dfd95d4cf1d950d04cb60b19abd9282929b
Ed Morley <501702+edmorley@users.noreply.github.com>
2024-03-01 16:08:56 +0000

    Enable `unused_crate_dependencies` lint (#222)
    For parity with our other CNBs.
    
    This lint wasn't enabled previously, due to many false positives from the
    binary utils having previously been part of the main crate - however, they've
    since been split out into the `go-utils` crate in this repo.
    
    Enabling this lint found some unused/mis-categorised deps, which have now
    been fixed.

--- common/go-utils/src/bin/diff_inventory.rs
+++ common/go-utils/src/bin/diff_inventory.rs
@@ -1,3 +1,6 @@
+// Required due to: https://github.com/rust-lang/rust/issues/95513
+#![allow(unused_crate_dependencies)]
+
 use heroku_go_utils::inv::{list_github_go_versions, Artifact, Inventory};
 use std::collections::HashSet;
 
