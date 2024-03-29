a431eb27c5fb7eedab566a6c17afe309c30cbd80
Rune Soerensen <rsoerensen@salesforce.com>
2024-03-29 05:37:33 -0400

    Separate Go and semantic version parsing

--- common/go-utils/src/vrs.rs
+++ common/go-utils/src/vrs.rs
@@ -9,11 +9,33 @@ use std::fmt;
 ///
 /// The derived `Default` implementation creates a wildcard version `Requirement`.
 #[derive(Debug, Default, PartialEq, Clone)]
-pub struct GoRequirement(semver::VersionReq);
+pub struct GoRequirement(SemanticVersionRequirement);
+
+#[derive(Debug, Default, PartialEq, Clone)]
+pub struct SemanticVersionRequirement(semver::VersionReq);
+
+impl fmt::Display for SemanticVersionRequirement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl VersionRequirement<SemanticVersion> for SemanticVersionRequirement {
+    fn satisfies(&self, version: &SemanticVersion) -> bool {
+        self.0.matches(&version.0)
+    }
+
+    fn parse(input: &str) -> Result<Self, RequirementParseError>
+    where
+        Self: Sized,
+    {
+        parse_semver_requirement(input).map(SemanticVersionRequirement)
+    }
+}
 
 impl VersionRequirement<GoVersion> for GoRequirement {
     fn satisfies<'a>(&self, version: &GoVersion) -> bool {
-        self.0.matches(&version.0)
+        self.0.satisfies(&version.0)
     }
 
     /// Parses a go version requirement `&str` as a `Requirement`
@@ -31,10 +53,10 @@ impl VersionRequirement<GoVersion> for GoRequirement {
     fn parse(input: &str) -> Result<Self, RequirementParseError> {
         input
             .strip_prefix("go")
-            .map_or(parse_semver_requirement(input), |req| {
-                parse_semver_requirement(format!("={req}").as_str())
+            .map_or(SemanticVersionRequirement::parse(input), |req| {
+                SemanticVersionRequirement::parse(format!("={req}").as_str())
             })
-            .map(Self)
+            .map(GoRequirement)
     }
 }
 
@@ -52,8 +74,51 @@ impl fmt::Display for GoRequirement {
 /// - `Deserialize` and `Serialize` traits
 /// - Ability to parse go-flavored versions
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct GoVersion(SemanticVersion);
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord)]
 #[serde(try_from = "String", into = "String")]
-pub struct GoVersion(semver::Version);
+pub struct SemanticVersion(semver::Version);
+
+impl fmt::Display for SemanticVersion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl TryFrom<String> for SemanticVersion {
+    type Error = SemanticVersionParseError;
+
+    fn try_from(val: String) -> Result<Self, Self::Error> {
+        SemanticVersion::parse(&val)
+    }
+}
+
+impl From<SemanticVersion> for String {
+    fn from(ver: SemanticVersion) -> Self {
+        format!("{ver}")
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum SemanticVersionParseError {
+    #[error("Couldn't parse semantic version: {0}")]
+    SemVer(#[from] semver::Error),
+}
+
+impl Version for SemanticVersion {
+    type Error = SemanticVersionParseError;
+
+    fn parse(version: &str) -> Result<Self, Self::Error> {
+        semver::Version::parse(version)
+            .map(SemanticVersion)
+            .map_err(SemanticVersionParseError::SemVer)
+    }
+
+    fn parse_go(go_version: &str) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
 
 #[derive(thiserror::Error, Debug)]
 pub enum GoVersionParseError {
@@ -63,6 +128,8 @@ pub enum GoVersionParseError {
     Regex(#[from] regex::Error),
     #[error("Couldn't parse version. Unable to capture values from regex.")]
     Captures,
+    #[error("Couldn't parse go version: {0}")]
+    SemanticVersion(#[from] SemanticVersionParseError),
 }
 
 impl Version for GoVersion {
@@ -76,9 +143,9 @@ impl Version for GoVersion {
     /// let req = heroku_go_utils::vrs::GoVersion::parse("1.14.2").unwrap();
     /// ```
     fn parse(version: &str) -> Result<Self, Self::Error> {
-        semver::Version::parse(version)
+        SemanticVersion::parse(version)
             .map(GoVersion)
-            .map_err(GoVersionParseError::SemVer)
+            .map_err(GoVersionParseError::SemanticVersion)
     }
 
     /// Parses a go version `&str` as a `Version`
