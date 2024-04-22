808d75478dd82ea9240017a4eda6e3635999ce77
Rune Soerensen <rsoerensen@salesforce.com>
2024-04-22 16:29:54 -0400

    Merge diff and update utils

--- common/go-utils/src/bin/update_inventory.rs
+++ common/go-utils/src/bin/update_inventory.rs
@@ -1,9 +1,10 @@
 // Required due to: https://github.com/rust-lang/rust/issues/95513
 #![allow(unused_crate_dependencies)]
 
-use heroku_go_utils::inv::list_upstream_artifacts;
-use heroku_inventory_utils::inv::Inventory;
-use std::{env, fs, process};
+use heroku_go_utils::{inv::list_upstream_artifacts, vrs::GoVersion};
+use heroku_inventory_utils::inv::{read_inventory_file, Artifact, Inventory};
+use sha2::Sha256;
+use std::{collections::HashSet, env, fs, process};
 
 /// Updates the local go inventory.toml with versions published on go.dev.
 fn main() {
@@ -11,6 +12,16 @@ fn main() {
         eprintln!("Usage: update_inventory <path/to/inventory.toml>");
         process::exit(2);
     });
+
+    let inventory_artifacts: HashSet<Artifact<GoVersion, Sha256>> =
+        read_inventory_file(&inventory_path)
+            .unwrap_or_else(|e| {
+                eprintln!("Error reading inventory at '{inventory_path}': {e}");
+                std::process::exit(1);
+            })
+            .artifacts
+            .into_iter()
+            .collect();
 
     // List available upstream release versions.
     let remote_artifacts = list_upstream_artifacts().unwrap_or_else(|e| {
@@ -30,5 +41,27 @@ fn main() {
     fs::write(inventory_path, toml).unwrap_or_else(|e| {
         eprintln!("Error writing inventory to file: {e}");
         process::exit(7);
+    });
+
+    let remote_artifacts: HashSet<Artifact<GoVersion, Sha256>> =
+        inventory.artifacts.into_iter().collect();
+
+    [
+        ("Added", &remote_artifacts - &inventory_artifacts),
+        ("Removed", &inventory_artifacts - &remote_artifacts),
+    ]
+    .iter()
+    .filter(|(_, artifact_diff)| !artifact_diff.is_empty())
+    .for_each(|(action, artifacts)| {
+        let mut list: Vec<&Artifact<GoVersion, Sha256>> = artifacts.iter().collect();
+        list.sort_by_key(|a| &a.version);
+        println!(
+            "{} {}.",
+            action,
+            list.iter()
+                .map(ToString::to_string)
+                .collect::<Vec<_>>()
+                .join(", ")
+        );
     });
 }
