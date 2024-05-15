55139961a87578b17ee963f275e1d6031f2ca658
Josh W Lewis <josh.lewis@salesforce.com>
2024-05-15 12:36:28 -0500

    Multi-arch integration tests

--- buildpacks/go/tests/integration_test.rs
+++ buildpacks/go/tests/integration_test.rs
@@ -4,43 +4,49 @@
 use libcnb_test::{assert_contains, assert_not_contains, BuildConfig, ContainerConfig, TestRunner};
 use std::time::Duration;
 
-fn test_go_fixture(
-    fixture: &str,
-    builder: &str,
-    expect_loglines: &[&str],
-    refute_loglines: &[&str],
-) {
-    TestRunner::default().build(
-        BuildConfig::new(builder, format!("tests/fixtures/{fixture}")),
-        |ctx| {
-            let logs = format!("{}\n{}", ctx.pack_stdout, ctx.pack_stderr);
-            for expect_line in expect_loglines {
-                assert_contains!(logs, expect_line);
-            }
-            for refute_line in refute_loglines {
-                assert_not_contains!(logs, refute_line);
-            }
+const DEFAULT_BUILDER: &str = "heroku/builder:24";
 
-            let port = 8080;
-            ctx.start_container(ContainerConfig::new().expose_port(port), |container| {
-                std::thread::sleep(Duration::from_secs(5));
-                let addr = container.address_for_port(port);
-                let resp = ureq::get(&format!("http://{addr}"))
-                    .call()
-                    .expect("request to container failed")
-                    .into_string()
-                    .expect("response read error");
+fn test_go_fixture(fixture: &str, expect_loglines: &[&str], refute_loglines: &[&str]) {
+    let builder = std::env::var("INTEGRATION_TEST_BUILDER").unwrap_or(DEFAULT_BUILDER.to_string());
+    // TODO: Once Pack build supports `--platform` and libcnb-test adjusted accordingly, change this
+    // to allow configuring the target arch independently of the builder name (eg via env var).
+    let target_triple = match builder.as_str() {
+        // Compile the buildpack for ARM64 iff the builder supports multi-arch and the host is ARM64.
+        "heroku/builder:24" if cfg!(target_arch = "aarch64") => "aarch64-unknown-linux-musl",
+        _ => "x86_64-unknown-linux-musl",
+    };
 
-                assert_contains!(resp, fixture);
-            });
-        },
-    );
+    let mut build_config = BuildConfig::new(builder, format!("tests/fixtures/{fixture}"));
+    build_config.target_triple(target_triple);
+    TestRunner::default().build(build_config, |ctx| {
+        let logs = format!("{}\n{}", ctx.pack_stdout, ctx.pack_stderr);
+        for expect_line in expect_loglines {
+            assert_contains!(logs, expect_line);
+        }
+        for refute_line in refute_loglines {
+            assert_not_contains!(logs, refute_line);
+        }
+
+        let port = 8080;
+        ctx.start_container(ContainerConfig::new().expose_port(port), |container| {
+            std::thread::sleep(Duration::from_secs(5));
+            let addr = container.address_for_port(port);
+            let resp = ureq::get(&format!("http://{addr}"))
+                .call()
+                .expect("request to container failed")
+                .into_string()
+                .expect("response read error");
+
+            assert_contains!(resp, fixture);
+        });
+    });
 }
 
-fn test_basic_http_116(builder: &str) {
+#[test]
+#[ignore = "integration test"]
+fn test_basic_http_116() {
     test_go_fixture(
         "basic_http_116",
-        builder,
         &[
             "Detected Go version requirement: ~1.16.2",
             "Resolved Go version: go1.16.",
@@ -49,21 +55,12 @@ fn test_basic_http_116(builder: &str) {
         &[],
     );
 }
-#[test]
-#[ignore = "integration test"]
-fn basic_http_116_20() {
-    test_basic_http_116("heroku/builder:20");
-}
-#[test]
-#[ignore = "integration test"]
-fn basic_http_116_22() {
-    test_basic_http_116("heroku/builder:22");
-}
 
-fn test_vendor_gorilla_117(builder: &str) {
+#[test]
+#[ignore = "integration test"]
+fn test_vendor_gorilla_117() {
     test_go_fixture(
         "vendor_gorilla_117",
-        builder,
         &[
             "Detected Go version requirement: =1.17.8",
             "Installing go1.17.8",
@@ -72,21 +69,12 @@ fn test_vendor_gorilla_117(builder: &str) {
         &["downloading github.com/gorilla/mux v1.8.0"],
     );
 }
-#[test]
-#[ignore = "integration test"]
-fn vendor_gorilla_117_20() {
-    test_vendor_gorilla_117("heroku/builder:20");
-}
-#[test]
-#[ignore = "integration test"]
-fn vendor_gorilla_117_22() {
-    test_vendor_gorilla_117("heroku/builder:22");
-}
 
-fn test_modules_gin_121(builder: &str) {
+#[test]
+#[ignore = "integration test"]
+fn test_modules_gin_121() {
     test_go_fixture(
         "modules_gin_121",
-        builder,
         &[
             "Detected Go version requirement: =1.21",
             "Installing go1.21",
@@ -95,21 +83,12 @@ fn test_modules_gin_121(builder: &str) {
         &[],
     );
 }
-#[test]
-#[ignore = "integration test"]
-fn modules_gin_121_20() {
-    test_modules_gin_121("heroku/builder:20");
-}
-#[test]
-#[ignore = "integration test"]
-fn modules_gin_121_22() {
-    test_modules_gin_121("heroku/builder:22");
-}
 
-fn test_worker_http_118(builder: &str) {
+#[test]
+#[ignore = "integration test"]
+fn test_worker_http_118() {
     test_go_fixture(
         "worker_http_118",
-        builder,
         &[
             "Detected Go version requirement: ~1.18.1",
             "Installing go1.18.",
@@ -119,21 +98,12 @@ fn test_worker_http_118(builder: &str) {
         &["example.com/worker_http_118/cmd/script"],
     );
 }
-#[test]
-#[ignore = "integration test"]
-fn worker_http_118_20() {
-    test_worker_http_118("heroku/builder:20");
-}
-#[test]
-#[ignore = "integration test"]
-fn worker_http_118_22() {
-    test_worker_http_118("heroku/builder:22");
-}
 
-fn test_basic_http_119(builder: &str) {
+#[test]
+#[ignore = "integration test"]
+fn test_basic_http_119() {
     test_go_fixture(
         "basic_http_119",
-        builder,
         &[
             "Detected Go version requirement: ~1.19.4",
             "Installing go1.19.",
@@ -141,21 +111,12 @@ fn test_basic_http_119(builder: &str) {
         &[],
     );
 }
-#[test]
-#[ignore = "integration test"]
-fn basic_http_119_20() {
-    test_basic_http_119("heroku/builder:20");
-}
-#[test]
-#[ignore = "integration test"]
-fn basic_http_119_22() {
-    test_basic_http_119("heroku/builder:22");
-}
 
-fn test_vendor_fasthttp_120(builder: &str) {
+#[test]
+#[ignore = "integration test"]
+fn test_vendor_fasthttp_120() {
     test_go_fixture(
         "vendor_fasthttp_120",
-        builder,
         &[
             "Detected Go version requirement: =1.20",
             "Installing go1.20.",
@@ -164,37 +125,18 @@ fn test_vendor_fasthttp_120(builder: &str) {
         &["downloading github.com/valyala/fasthttp"],
     );
 }
-#[test]
-#[ignore = "integration test"]
-fn vendor_fasthttp_120_20() {
-    test_vendor_fasthttp_120("heroku/builder:20");
-}
-#[test]
-#[ignore = "integration test"]
-fn vendor_fasthttp_120_22() {
-    test_vendor_fasthttp_120("heroku/builder:22");
-}
 
-fn test_basic_http_122(builder: &str) {
+#[test]
+#[ignore = "integration test"]
+fn test_basic_http_122() {
     test_go_fixture(
         "basic_http_122",
-        builder,
         &[
             "Detected Go version requirement: ~1.22.0",
             "Installing go1.22.",
         ],
         &[],
     );
-}
-#[test]
-#[ignore = "integration test"]
-fn basic_http_122_20() {
-    test_basic_http_122("heroku/builder:20");
-}
-#[test]
-#[ignore = "integration test"]
-fn basic_http_122_22() {
-    test_basic_http_122("heroku/builder:22");
 }
 
 #[test]
@@ -203,13 +145,10 @@ fn test_go_artifact_caching() {
     TestRunner::default().build(
         BuildConfig::new("heroku/builder:22", "tests/fixtures/basic_http_116"),
         |ctx| {
-            assert_contains!(
-                ctx.pack_stdout,
-                "Installing go1.16.15 (linux-amd64) from https://go.dev/dl/go1.16.15.linux-amd64.tar.gz",
-            );
+            assert_contains!(ctx.pack_stdout, "Installing go1.16.",);
             let config = ctx.config.clone();
             ctx.rebuild(config, |ctx| {
-                assert_contains!(ctx.pack_stdout, "Reusing go1.16.15 (linux-amd64)");
+                assert_contains!(ctx.pack_stdout, "Reusing go1.16.");
             });
         },
     );
