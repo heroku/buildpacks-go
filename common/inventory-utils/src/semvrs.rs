ad6fa4a218ce136a4fccf1686818097b6692484e
Rune Soerensen <rsoerensen@salesforce.com>
2024-09-24 12:17:25 -0400

    Delete inventory-utils

--- common/inventory-utils/src/semvrs.rs
+++
@@ -1,7 +0,0 @@
-use crate::inv::VersionRequirement;
-
-impl VersionRequirement<semver::Version> for semver::VersionReq {
-    fn satisfies(&self, version: &semver::Version) -> bool {
-        self.matches(version)
-    }
-}
