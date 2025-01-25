1b98474755a0629b0c260d5cd2037058762645c8
Schneems <richard.schneeman+foo@gmail.com>
2025-01-25 13:51:52 -0600

    Update command running format

--- buildpacks/go/src/cmd.rs
+++ buildpacks/go/src/cmd.rs
@@ -1,12 +1,15 @@
+use bullet_stream::{global::print, style};
+use fun_run::{CmdError, CommandWithName};
 use libcnb::Env;
-use std::process::{Command, ExitStatus, Stdio};
+use std::process::Command;
 
 #[derive(thiserror::Error, Debug)]
 pub(crate) enum Error {
+    #[error("{0}")]
+    FailedCommand(#[from] CmdError),
+
     #[error("Command IO error: {0}")]
     IO(#[from] std::io::Error),
-    #[error("Command did not exit successfully: {0}")]
-    Exit(ExitStatus),
 }
 
 /// Run `go install -tags heroku pkg [..pkgn]`. Useful for compiling a list
@@ -22,8 +25,15 @@ pub(crate) fn go_install<S: AsRef<str>>(packages: &[S], go_env: &Env) -> Result<
     for pkg in packages {
         args.push(pkg.as_ref());
     }
-    let status = Command::new("go").args(args).envs(go_env).status()?;
-    status.success().then_some(()).ok_or(Error::Exit(status))
+    let mut cmd = Command::new("go");
+    cmd.args(args).envs(go_env);
+
+    print::sub_stream_with(
+        format!("Running {}", style::command(cmd.name())),
+        |stdout, stderr| cmd.stream_output(stdout, stderr),
+    )
+    .map(|_| ())
+    .map_err(Error::FailedCommand)
 }
 
 /// Run `go list -tags -f {{ .ImportPath }} ./...`. Useful for listing
@@ -36,28 +46,27 @@ pub(crate) fn go_install<S: AsRef<str>>(packages: &[S], go_env: &Env) -> Result<
 /// Returns an error if the command exit code is not 0 or if there is an IO
 /// issue with the command.
 pub(crate) fn go_list(go_env: &Env) -> Result<Vec<String>, Error> {
-    let result = Command::new("go")
-        .args(vec![
-            "list",
-            "-tags",
-            "heroku",
-            "-f",
-            "{{ if eq .Name \"main\" }}{{ .ImportPath }}{{ end }}",
-            "./...",
-        ])
-        .envs(go_env)
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::piped())
-        .output()?;
+    let mut cmd = Command::new("go");
+    cmd.args(vec![
+        "list",
+        "-tags",
+        "heroku",
+        "-f",
+        "{{ if eq .Name \"main\" }}{{ .ImportPath }}{{ end }}",
+        "./...",
+    ])
+    .envs(go_env);
 
-    result
-        .status
-        .success()
-        .then_some(())
-        .ok_or(Error::Exit(result.status))?;
-
-    Ok(String::from_utf8_lossy(&result.stdout)
-        .split_whitespace()
-        .map(|s| s.trim().to_string())
-        .collect())
+    print::sub_stream_with(
+        format!("Running {}", style::command(cmd.name())),
+        |stdout, stderr| cmd.stream_output(stdout, stderr),
+    )
+    .map_err(Error::FailedCommand)
+    .map(|output| {
+        output
+            .stdout_lossy()
+            .split_whitespace()
+            .map(|s| s.trim().to_string())
+            .collect()
+    })
 }
