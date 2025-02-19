b0017de8a19b17f80e5a93e267f78cbde1da0df2
Josh W Lewis <josh.w.lewis@gmail.com>
2025-02-19 10:27:41 -0600

    Drop go_clean, which is no longer used

--- buildpacks/go/src/cmd.rs
+++ buildpacks/go/src/cmd.rs
@@ -9,21 +9,6 @@ pub(crate) enum Error {
     Exit(ExitStatus),
 }
 
-/// Run `go clean -tags heroku`. Useful for clearing the modcache or buildcache.
-///
-/// # Errors
-///
-/// Returns an error of the command exit code is not 0 or if there is an IO
-/// issue with the command.
-pub(crate) fn go_clean<S: AsRef<str>>(flag: S, go_env: &Env) -> Result<(), Error> {
-    let status = Command::new("go")
-        .args(["clean", "-tags", "heroku", flag.as_ref()])
-        .envs(go_env)
-        .status()?;
-
-    status.success().then_some(()).ok_or(Error::Exit(status))
-}
-
 /// Run `go install -tags heroku pkg [..pkgn]`. Useful for compiling a list
 /// of packages and installing each of them in `GOBIN`. This command is module
 /// aware, and will download required modules as a side-effect.
