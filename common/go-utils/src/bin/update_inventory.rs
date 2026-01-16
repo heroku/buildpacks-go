9d5d3eb070458473af6d0822b0d94b2d1707b691
Rune Soerensen <rsoerensen@salesforce.com>
2026-01-15 23:49:13 -0500

    Write changelog entries in `update_inventory`

--- common/go-utils/src/bin/update_inventory.rs
+++ common/go-utils/src/bin/update_inventory.rs
@@ -2,16 +2,21 @@
 #![allow(unused_crate_dependencies)]
 
 use heroku_go_utils::{inv::list_upstream_artifacts, vrs::GoVersion};
-use libherokubuildpack::inventory::Inventory;
+use keep_a_changelog_file::{ChangeGroup, Changelog};
+use libherokubuildpack::inventory::{Inventory, artifact::Artifact};
 use sha2::Sha256;
-use std::{env, fs, process};
+use std::{env, fs, process, str::FromStr};
 
 /// Updates the local go inventory.toml with versions published on go.dev.
 fn main() {
-    let inventory_path = env::args().nth(1).unwrap_or_else(|| {
-        eprintln!("Usage: update_inventory <path/to/inventory.toml>");
-        process::exit(2);
-    });
+    let (inventory_path, changelog_path) = {
+        let args: Vec<String> = env::args().collect();
+        if args.len() != 3 {
+            eprintln!("Usage: update_inventory <path/to/inventory.toml> <path/to/CHANGELOG.md>");
+            process::exit(2);
+        }
+        (args[1].clone(), args[2].clone())
+    };
 
     let old_inventory = fs::read_to_string(&inventory_path)
         .unwrap_or_else(|e| {
@@ -39,9 +44,35 @@ fn main() {
         process::exit(6);
     });
 
-    fs::write(inventory_path, toml).unwrap_or_else(|e| {
+    fs::write(&inventory_path, toml).unwrap_or_else(|e| {
         eprintln!("Error writing inventory to file: {e}");
         process::exit(7);
+    });
+
+    let changelog_contents = fs::read_to_string(&changelog_path).unwrap_or_else(|e| {
+        eprintln!("Error reading changelog at '{changelog_path}': {e}");
+        process::exit(8);
+    });
+
+    let mut changelog = Changelog::from_str(&changelog_contents).unwrap_or_else(|e| {
+        eprintln!("Error parsing changelog at '{changelog_path}': {e}");
+        process::exit(9);
+    });
+
+    update_changelog(
+        &mut changelog,
+        ChangeGroup::Added,
+        &difference(&new_inventory.artifacts, &old_inventory.artifacts),
+    );
+    update_changelog(
+        &mut changelog,
+        ChangeGroup::Removed,
+        &difference(&old_inventory.artifacts, &new_inventory.artifacts),
+    );
+
+    fs::write(&changelog_path, changelog.to_string()).unwrap_or_else(|e| {
+        eprintln!("Failed to write to changelog: {e}");
+        process::exit(10);
     });
 
     for (action, artifacts) in [
@@ -74,4 +105,26 @@ fn main() {
 /// Finds the difference between two slices.
 fn difference<'a, T: Eq>(a: &'a [T], b: &'a [T]) -> Vec<&'a T> {
     a.iter().filter(|&artifact| !b.contains(artifact)).collect()
+}
+
+/// Helper function to update the changelog.
+fn update_changelog(
+    changelog: &mut Changelog,
+    change_group: ChangeGroup,
+    artifacts: &[&Artifact<GoVersion, Sha256, Option<()>>],
+) {
+    if !artifacts.is_empty() {
+        let mut versions: Vec<_> = artifacts.iter().map(|artifact| &artifact.version).collect();
+        versions.sort_unstable();
+        versions.dedup();
+        let versions_str = versions
+            .iter()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        changelog
+            .unreleased
+            .add(change_group, format!("Support for {versions_str}."));
+    }
 }
